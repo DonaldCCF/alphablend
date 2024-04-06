4c22d43cbe31bf1d4efa832b3717980feaebbe1c
donaldcheng <117000928+donaldccf@users.noreply.github.com>
2024-04-06 17:40:13 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+"""Genetic Programming in Python, with a scikit-learn inspired API"""
+
+from setuptools import setup, find_packages
+import factorgene
+
+DESCRIPTION = __doc__
+VERSION = factorgene.__version__
+
+setup(name='factorgene',
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=open("README.md").read(),
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Topic :: Software Development',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10'],
+      author='Donald Cheng',
+      author_email='cu30ccf@gmail.com',
+      url='https://github.com/donaldccf/FactorGene',
+      license='new BSD',
+      packages=find_packages(exclude=['*.tests',
+                                      '*.tests.*']),
+      zip_safe=False,
+      package_data={'': ['LICENSE']},
+      install_requires=['scikit-learn>=1.0.2',
+                        'joblib>=1.0.0'])
