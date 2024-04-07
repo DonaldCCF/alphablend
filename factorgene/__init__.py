9e3e6eff1994690981fa0454a9796f0e39e23521
donaldcheng <117000928+donaldccf@users.noreply.github.com>
2024-04-07 11:02:11 +0800

    Delete factorgene directory

--- factorgene/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['GeneticOptimizer',
-           'make_function',
-           'function_map']
-
-from ._gene import GeneticOptimizer
-from ._func import make_function
-from ._map import function_map
